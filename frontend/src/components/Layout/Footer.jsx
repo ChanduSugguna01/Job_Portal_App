197f9910d5a35b0f028d711533664dadf9e6a15c
ChanduSugguna01 <chandusugguna@gmail.com>
2025-07-11 09:45:50 +0530

    Initial commit

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import {Context} from "../../main"
+import {Link} from "react-router-dom"
+import { FaGithub , FaLinkedin} from "react-icons/fa"
+import { SiLeetcode } from "react-icons/si";
+import { RiInstagramFill} from "react-icons/ri"
+function Footer() {
+  const {isAuthorized}  = useContext(Context)
+  return (
+    <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
+<div>&copy; All Rights Reserved by ChnaduSugguna.</div>
+<div>
+  <Link to={'https://github.com/ChanduSugguna01'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://www.linkedin.com/in/hema-chandu-sugguna-a1a941251/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+  <Link to={'https://www.instagram.com/chandu_sugguna/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+</div>
+      
+    </footer>
+  )
+}
+
+export default Footer
